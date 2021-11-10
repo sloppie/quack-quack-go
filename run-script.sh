e2fd8a24ff8250f797e2fb1848b952250e09748a
Gitau Nyanjui <34423900+sloppie@users.noreply.github.com>
2021-11-11 01:05:24 +0300

    [UPDATE]: Updated shell script instructions

--- run-script.sh
+++ run-script.sh
@@ -1,4 +1,4 @@
 echo "running web scrapper..."
 screen -S s1
 
-Xvfb :99 -ac & DISPLAY=:99 python3 scrapper.py
+Xvfb :99 -ac & DISPLAY=:99 python3 scrapper.py -i /home/sloppie/scrapper/migrator/viable_hotels_pictures_settings.json -e -it Photograph -is Large -ssf Strict
